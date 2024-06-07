5755bbdbaa0762dd797b7800cb9a18b4b5917768
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2024-06-07 13:08:15 +0900

    Add documentation

--- recognition/utils.py
+++ recognition/utils.py
@@ -1,9 +1,16 @@
 _debug = False
 
 def init(debug):
+    """
+    If DEBUG is True, enable debugging.
+    """
     global _debug
     _debug = debug
 
 def debug(*args):
+    """
+    Print ARGS, forwarding all of them to the 'print' function, but only if
+    debug is enabled.
+    """
     if _debug:
         print(*args)
