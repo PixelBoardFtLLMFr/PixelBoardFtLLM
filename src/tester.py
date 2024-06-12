90c6b369440202a2274d81c2db7205e5a4841a26
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2024-06-12 11:30:17 +0900

    Add options to tester

--- src/tester.py
+++ src/tester.py
@@ -1,5 +1,6 @@
 import serial
 import argparse
+import time
 
 def light_up_tile(ser, index, color):
     out = ""
@@ -7,18 +8,55 @@ def light_up_tile(ser, index, color):
         out += f"{index*20 + i},{color}\n"
     ser.write(out.encode('ascii'))
 
+def light_up_column(ser, index, color, delay):
+    for j in range(5):
+        light_up_tile(ser, index*5 + j, color)
+        time.sleep(delay)
+
+def display_france(ser, delay):
+    blue = "#0000ff"
+    white = "#ffffe0"
+    red = "#ff0000"
+
+    for i in range(2):
+        light_up_column(ser, i, blue, delay)
+
+    light_up_column(ser, 2, white, delay)
+
+    for i in range(3, 5):
+        light_up_column(ser, i, red, 1.5*delay)
+
+def light_off(ser, delay):
+    for i in range(5):
+        light_up_column(ser, i, "#000000", delay)
+
 arg_parser = argparse.ArgumentParser(prog="tester", description="Test the pixel board.")
 arg_parser.add_argument("-p", "--port", action='store', default="/dev/ttyACM0",
                         help="pixel board serial port")
 arg_parser.add_argument("-c", "--color", action='store', default="#ffffff",
                         help="color to give the pixels")
-arg_parser.add_argument("indices", help="indices of tiles to light up, can be either"
+arg_parser.add_argument("-i", "--index", action='store', default=None,
+                        help="indices of tiles to light up, can be either"
                         + "a single index like 0 or an interval like 0-4")
+arg_parser.add_argument("-f", "--france", action='store_true', default=False,
+                        help="display the French flag")
+arg_parser.add_argument("-o", "--off", action='store_true', default=False,
+                        help="turn off the pixel board")
+arg_parser.add_argument("-d", "--delay", action='store', default=0.5,
+                        type=float, help="delay between tiles")
 args = arg_parser.parse_args()
 
 ser = serial.Serial(args.port)
 
-indices = [int(x) for x in args.indices.split("-")]
+if args.off:
+    light_off(ser, args.delay)
+    exit(0)
+
+if args.france:
+    display_france(ser, args.delay)
+    exit(0)
+
+indices = [int(x) for x in args.index.split("-")]
 length = len(indices)
 
 if length == 1:
