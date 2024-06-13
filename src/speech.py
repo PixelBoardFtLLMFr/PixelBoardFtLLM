410aa13b8944c690970f2dc6c00898ba20803028
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2024-06-13 14:27:17 +0900

    Add language as parameter

--- src/speech.py
+++ src/speech.py
@@ -1,21 +1,30 @@
 import speech_recognition as sr
 
+class Lang:
+    EN = "en-US"
+    JP = "ja-JP"
+    FR = "fr-FR"
+    ID = "id-ID"
+
 class SpeechToText:
-    def __init__(self):
+    def __init__(self, lang=Lang.EN):
+        self.lang = lang
+
         self.recognizer = sr.Recognizer()
         self.microphone = sr.Microphone()
         self.microphone_stream = self.microphone.__enter__()
         self.recognizer.adjust_for_ambient_noise(self.microphone_stream)
-    
+
     def __del__(self):
         self.microphone_stream.__exit__(None, None, None)
+
+    def set_lang(self, lang):
+        self.lang = lang
 
     def listen(self):
         try:
             audio_data = self.recognizer.listen(self.microphone_stream)
-            text = self.recognizer.recognize_google(audio_data, language='en-US')
+            text = self.recognizer.recognize_google(audio_data, language=self.lang)
             return text
-        except sr.UnknownValueError:
-            return None
-        except sr.RequestError as e:
+        except:
             return None
