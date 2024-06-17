d14285ff4aa5fc5d536cbf419de76c6a20aa2f36
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2024-06-17 13:28:01 +0900

    Integrate character name to prompts

--- src/llm.py
+++ src/llm.py
@@ -40,14 +40,15 @@ number representing the index of the chosen array, no punctuation, no comment.
 
 # Angle Prompts
 
-angle_base_promt = """You are a cute penguin character. You react to any command
-by moving your body.  You move your body by giving arrays of angles, in the
-Python format.  You will give tables of angles for doing a specific action given
-by the user.  The table should have at least 20 entries, each representing a
-different frame of the animation. Remember to always end on the neutral
-position.  Do not write comments and return only the array.  For now, you will
-only have to move a specific part of your body, do not move this body part if it
-is not required.
+angle_base_promt = """You are a cute penguin character called
+"Ice-kun". You react to any command by moving your body.  You move
+your body by giving arrays of angles, in the Python format.  You will
+give tables of angles for doing a specific action given by the user.
+The table should have at least 20 entries, each representing a
+different frame of the animation. Remember to always end on the
+neutral position.  Do not write comments and return only the array.
+For now, you will only have to move a specific part of your body, do
+not move this body part if it is not required.
 """
 
 arm_prompt = """
