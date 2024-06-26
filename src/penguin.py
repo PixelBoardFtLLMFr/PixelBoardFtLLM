c0ada5b4fa0c3927ec44dbe6e9dd0a8a79034fd1
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2024-06-26 17:01:28 +0900

    Fix index bug

--- src/penguin.py
+++ src/penguin.py
@@ -347,7 +347,7 @@ class Penguin:
                     if pixel[3] != 0:
                         # non-transparent
                         final_y = (y0 + y + self.particle_pos)%self.size
-                        self.image.putpixel((min(x0 + x, self.size), final_y), pixel)
+                        self.image.putpixel((min(x0 + x, self.size - 1), final_y), pixel)
             
 
     def get_pixels(self):
