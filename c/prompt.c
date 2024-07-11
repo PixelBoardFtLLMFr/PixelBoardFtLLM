7a90a845318db1fd34e9cbe41075b037eab34e5a
Tristan Riehs <tristan.riehs@bordeaux-inp.fr>
2024-07-11 19:55:45 +0900

    Add high-level funcion for building all prompts

--- c/prompt.c
+++ c/prompt.c
@@ -3,9 +3,32 @@
 #include <stdio.h>
 
 #include "prompt.h"
+#include "llm.h"
 
 /* 4KB */
 #define MAX_FILE_SIZE (1 << 12)
+
+enum prompt_type {
+	PT_ARM,
+	PT_LEG,
+	PT_HEAD,
+	PT_FACE,
+	PT_PARTICLE,
+	PT_EYE,
+	PT_HEIGHT,
+	PT_COUNT,
+	PT_FIRST = PT_ARM,
+};
+
+static char *keys[] = {
+	[PT_ARM] = "ARM",
+	[PT_LEG] = "LEG",
+	[PT_HEAD] = "HEAD",
+	[PT_FACE] = "FACE",
+	[PT_PARTICLE] = "PARTICLE",
+	[PT_EYE] = "EYE",
+	[PT_HEIGHT] = "HEIGHT",
+};
 
 static char *angle_base;
 
@@ -112,7 +135,9 @@ static char *concat(const char *s0, const char *s1, const char *s2)
 	return strdup(buf);
 }
 
-char *prompt_sys(enum prompt_type type)
+/* Get the "system" part of the prompt for the given TYPE. The returned
+ string is heap-allocated. */
+static char *prompt_sys(enum prompt_type type)
 {
 	switch (type) {
 	case PT_ARM:
@@ -137,10 +162,25 @@ char *prompt_sys(enum prompt_type type)
 	return NULL;
 }
 
-char *prompt_user(enum prompt_type type, const char *input)
+/* Get the "user" part of the prompt for the given TYPE and the
+   INPUT given by the end-user. The returned string is heap-allocated. */
+static char *prompt_usr(enum prompt_type type, const char *input)
 {
 	char buf[MAX_FILE_SIZE];
 	strcpy(buf, "The user input is : ");
 	strncat(buf, input, MAX_FILE_SIZE - strlen(input) - 1);
 	return strdup(buf);
+}
+
+struct json_object *prompt_execute_all(const char *key, const char *input)
+{
+	struct llm_ctx *ctx = llm_init(key, "gpt-3.5-turbo");
+
+	for (int i=0; i < PT_COUNT; i++) {
+		char *sys = prompt_sys(i);
+		char *usr = prompt_usr(i, input);
+		llm_push_prompt(ctx, keys[i], sys, usr);
+	}
+
+	return llm_execute_prompts(ctx);
 }
--- c/prompt.h
+++ c/prompt.h
@@ -1,15 +1,7 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
-enum prompt_type {
-	PT_ARM,
-	PT_LEG,
-	PT_HEAD,
-	PT_FACE,
-	PT_PARTICLE,
-	PT_EYE,
-	PT_HEIGHT,
-};
+#include <json_object.h>
 
 /* Should be called before any other function for initializing
    the prompt builder. */
@@ -17,12 +9,7 @@ void prompt_init(void);
 
 void prompt_destroy(void);
 
-/* Get the "system" part of the prompt for the given TYPE. The returned
- string is heap-allocated. */
-char *prompt_sys(enum prompt_type type);
-
-/* Get the "user" part of the prompt for the given TYPE and the
-   INPUT given by the end-user. The returned string is heap-allocated. */
-char *prompt_user(enum prompt_type type, const char *input);
+/* Build all the needed prompts, execute them all, and return the results. */
+struct json_object *prompt_execute_all(const char *key, const char *input);
 
 #endif
